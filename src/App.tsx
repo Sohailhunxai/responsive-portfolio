03e8448c8e8f14d92766c15a0421eaa2e9b65161
Sohail Abbas <sohalidatoo@gmail.com>
2023-11-17 18:53:08 +0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+enum SelectedPage {
+Home = 'home',
+Benefits = 'benefits',
+OurClasses = 'ourclasses',
+ContactUs = 'contactus'
+}
 
 function App() {
+  const [selectedPage , setSelectedPage] = useState<SelectedPage>("home")
   return (
     <div className=" app  bg-gray-20 ">
-   <Navbar/>
+   <Navbar 
+   selectedPage ={selectedPage}
+         setSelectedPage = {setSelectedPage}/>
     </div>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect , useState } from 'react';
 import  { Bars3Icon , XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from './Link';
 
 
-type Props = {}
+type Props = {
+    selectedPage: string,
+  setSelectedPage:(value:string)=> void;
+}
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
   const flexBetween = " flex items-center justify-between";
   return (
 <nav>
@@ -17,10 +21,31 @@ const Navbar = (props: Props) => {
     {/*rightt side */}
       <div  className={`${flexBetween} w-full `}>
         <div className={`${flexBetween} gap-8 text-sm`}>
-          <p>Home</p>
-          <p>Benefits</p>
-          <p>Our Classes</p>
-          <p>Contact Us</p>
+         <Link 
+         page=" Home" 
+         selectedPage ={selectedPage}
+         setSelectedPage = {setSelectedPage}
+         />
+         
+         
+         <Link  page="Benefits"
+         selectedPage ={selectedPage}
+         setSelectedPage = {setSelectedPage}/>
+         
+         
+         <Link 
+         page="Our Classes"
+         selectedPage ={selectedPage}
+         setSelectedPage = {setSelectedPage}
+         /> 
+         
+         
+         <Link 
+         page="Contact Us" 
+         selectedPage ={selectedPage}
+         setSelectedPage = {setSelectedPage}
+         /> 
+        
         </div>
         <div className={`${flexBetween} gap-8`}>
           <p>Sign In</p>
