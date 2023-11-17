d9befbc0655abd9001e00c9a79ef72e113ba0a0b
Sohail Abbas <sohalidatoo@gmail.com>
2023-11-17 15:57:51 +0500

    Navbar Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Navbar from "@/scenes/navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div className=" app  bg-gray-20 ">
+   <Navbar/>
+    </div>
   );
 }
 
