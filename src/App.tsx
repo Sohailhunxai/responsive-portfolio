37ae9e159bcda9892eb0a3c9620002b6ce04bd8a
Sohail Abbas <sohalidatoo@gmail.com>
2023-11-17 12:33:16 +0500

    My First Commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
