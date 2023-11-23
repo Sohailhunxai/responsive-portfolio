505198c1bea3b3c9634ce02362374a2995346327
Sohail Abbas <sohalidatoo@gmail.com>
2023-11-23 13:59:49 +0500

    Benefits page Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
-import Benefits from "@/scenes/benefits"
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Benefits from "./scenes/benefits";
 
 
 function App() {
--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,17 +1,40 @@
-import { SelectedPage } from "@/shared/types"
+import HText from "@/shared/HText";
+import {BenefitType, SelectedPage } from "@/shared/types"
 import { 
 HomeModernIcon, 
 UserGroupIcon ,
 AcademicCapIcon} from "@heroicons/react/24/solid"
 import { motion } from "framer-motion";
+import Benefit from "./Benefit";
 
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+]
 
 type Props = {
 setSelectedPage:(value:SelectedPage)=> void;
 
-}
+};
 
-const Benefits = (setSelectedPage: Props) => {
+const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section 
     id="benefits"
@@ -20,6 +43,27 @@ const Benefits = (setSelectedPage: Props) => {
       onViewportEnter={()=> setSelectedPage(SelectedPage.Benefits)}
       >
 
+        {/*HEADER */}
+        <div className="md:my-5 md:w-3/5">
+        <HText>
+          MORE THAN JUST GYM.
+        </HText>
+          <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.</p>
+        </div>
+        {/*BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit: BenefitType)=>(
+            <Benefit
+            key={benefit.title}
+            icon ={benefit.icon}
+            title = {benefit.title}
+            description = {benefit.description}
+            setSelectedPage = {setSelectedPage}
+            />
+          ))}
+        </div>
       </motion.div>
     </section>
   )
