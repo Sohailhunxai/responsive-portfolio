71ea7b405dd8259e77213af6c8bd18e5b3f244a1
Sohail Abbas <sohalidatoo@gmail.com>
2023-11-27 13:08:03 +0500

    Css Issue Fixed

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -6,7 +6,7 @@ image: string;
 }
 
 const Class = ({name, description, image}: Props) => {
-  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition-500 hover:opacity-90`;
+  const overlayStyles = `p-5 absolute z-10 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition-500 hover:opacity-90`;
 
   return( 
   <li className="relative mx-5 inline-block h-[380px] w-[450px]">
